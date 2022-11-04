e479c4b64a90fc8c647b96d1f7429bdcdf65ab34
kd1042 <73589096+kd1042@users.noreply.github.com>
2022-11-04 20:36:56 +0530

    layout updated

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.bt');
+let password = document.getElementById('password').value;
+let confpass = document.getElementById('confirm-password').value;
+
+button.addEventListener('click', () => {
+     
+    console.log(password);
+
+})
